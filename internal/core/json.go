512f6795fc8418454d387b5f94714cf46a67c5d1
GraphR00t <113632189+GraphR00t@users.noreply.github.com>
2023-04-02 16:35:09 +0200

    json: refactor some marshaling helpers

--- internal/core/json.go
+++ internal/core/json.go
@@ -105,10 +105,23 @@ func parseJson(ctx *Context, v any) (any, error) {
 }
 
 func MarshalJsonNoHTMLEspace(v any) ([]byte, error) {
+	return marshalJsonNoHTMLEspace(v)
+}
+
+func marshalJsonNoHTMLEspace(v any, encoderOptions ...func(encoder *json.Encoder)) ([]byte, error) {
+	//create encoder
+
 	buf := bytes.NewBuffer(nil)
 	encoder := json.NewEncoder(buf)
 	encoder.SetEscapeHTML(false)
+	for _, opt := range encoderOptions {
+		opt(encoder)
+	}
+
+	//encode
+
 	err := encoder.Encode(v)
+
 	if err != nil {
 		return nil, err
 	}
@@ -116,16 +129,9 @@ func MarshalJsonNoHTMLEspace(v any) ([]byte, error) {
 	//remove newline
 	return bytes[:len(bytes)-1], nil
 }
+
 func MarshalIndentJsonNoHTMLEspace(v any, prefix, indent string) ([]byte, error) {
-	buf := bytes.NewBuffer(nil)
-	encoder := json.NewEncoder(buf)
-	encoder.SetEscapeHTML(false)
-	encoder.SetIndent(prefix, indent)
-	err := encoder.Encode(v)
-	if err != nil {
-		return nil, err
-	}
-	bytes := buf.Bytes()
-	//remove newline
-	return bytes[:len(bytes)-1], nil
+	return marshalJsonNoHTMLEspace(v, func(encoder *json.Encoder) {
+		encoder.SetIndent(prefix, indent)
+	})
 }
