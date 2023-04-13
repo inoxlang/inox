ab10011a3aed595091ce591f1b0112387d74db4a
GraphR00t <113632189+GraphR00t@users.noreply.github.com>
2023-04-13 18:59:27 +0200

    parse: add test for parsedChunk.GetNodeAtSpan

---
+++ internal/parse/module_source_test.go
@@ -0,0 +1,30 @@
+package internal
+
+import (
+	"testing"
+
+	"github.com/inox-project/inox/internal/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetNodeAtSpan(t *testing.T) {
+
+	chunk := utils.Must(ParseChunkSource(InMemorySource{
+		NameString: "test",
+		CodeString: "a = 1\na\nfn f(){}",
+	}))
+
+	span := chunk.GetLineColumnSingeCharSpan(1, 1)
+	node, ok := chunk.GetNodeAtSpan(span)
+	if !assert.True(t, ok) {
+		return
+	}
+	assert.IsType(t, &IdentifierLiteral{}, node)
+
+	span = chunk.GetLineColumnSingeCharSpan(2, 1)
+	node, ok = chunk.GetNodeAtSpan(span)
+	if !assert.True(t, ok) {
+		return
+	}
+	assert.IsType(t, &IdentifierLiteral{}, node)
+}
