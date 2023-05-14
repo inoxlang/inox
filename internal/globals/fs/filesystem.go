161f622816312dae355fb4d6a1ca25b55fd1c138
GraphR00t <113632189+GraphR00t@users.noreply.github.com>
2023-05-14 22:24:44 +0200

    fs: fix memory filesystem

--- internal/globals/fs/filesystem.go
+++ internal/globals/fs/filesystem.go
@@ -5,7 +5,6 @@ import (
 	"path/filepath"
 
 	"github.com/go-git/go-billy/v5"
-	"github.com/go-git/go-billy/v5/memfs"
 	"github.com/go-git/go-billy/v5/osfs"
 	afs "github.com/inoxlang/inox/internal/afs"
 
@@ -43,26 +42,4 @@ func (fs OsFilesystem) Root() string {
 
 func (fs OsFilesystem) Absolute(path string) (string, error) {
 	return filepath.Abs(path)
-}
-
-type MemFilesystem struct {
-	billy.Filesystem
-}
-
-func NewMemFilesystem() *MemFilesystem {
-	return &MemFilesystem{
-		Filesystem: memfs.New(),
-	}
-}
-
-func (fs MemFilesystem) Chroot(path string) (billy.Filesystem, error) {
-	return nil, core.ErrNotImplemented
-}
-
-func (fs MemFilesystem) Root() string {
-	panic(core.ErrNotImplemented)
-}
-
-func (fs MemFilesystem) Absolute(path string) (string, error) {
-	return "", core.ErrNotImplemented
 }
