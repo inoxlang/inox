d20831f840c02d7298cd5101c9cd132e382081f2
GraphR00t <113632189+GraphR00t@users.noreply.github.com>
2023-05-26 14:42:07 +0200

    chrome: add empty impl. for WASM on JS

--- internal/globals/chrome/handle.go
+++ internal/globals/chrome/handle.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"time"
 
-	"github.com/chromedp/chromedp"
-
 	core "github.com/inoxlang/inox/internal/core"
 	_html "github.com/inoxlang/inox/internal/globals/html"
 )
@@ -30,117 +28,36 @@ type Handle struct {
 }
 
 func NewHandle(ctx *core.Context) (*Handle, error) {
-	logger := *ctx.Logger()
-	logger = logger.With().Str(core.SOURCE_LOG_FIELD_NAME, SRC_PATH).Logger()
-
-	opts := append(chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.DisableGPU,
-		chromedp.Flag("headless", true),
-		chromedp.UserDataDir(string(core.CreateTempdir("chrome", ctx.GetFileSystem()))),
-		//chromedp.Headless,
-	)
-
-	allocCtx, cancelAllocCtx := chromedp.NewExecAllocator(ctx, opts...)
-
-	chromedpCtx, cancel := chromedp.NewContext(
-		allocCtx,
-		chromedp.WithErrorf(func(s string, i ...interface{}) {
-			logger.Warn().Msgf(s, i...)
-		}),
-		chromedp.WithLogf(func(s string, i ...interface{}) {
-			logger.Info().Msgf(s, i...)
-		}),
-		//most chatty
-		chromedp.WithDebugf(func(s string, i ...interface{}) {
-			logger.Trace().Msgf(s, i...)
-		}),
-
-		//???: the previous options are not working if they are created as several chromedp.WithBrowserXXX calls in chromedp.WithBrowserOption(...)
-
-		//chromedp.WithDebugf(ctx.GetState().Logger.Printf),
-	)
-
-	handle := &Handle{
-		allocCtx:       allocCtx,
-		cancelAllocCtx: cancelAllocCtx,
-
-		chromedpContext:       chromedpCtx,
-		cancelChromedpContext: cancel,
-	}
-
-	if err := handle.do(ctx, chromedp.EmulateViewport(1920, 1080)); err != nil {
+	handle := newHandle(ctx)
+	if err := handle.doEmulateViewPort(ctx); err != nil {
 		return nil, err
 	}
 
 	return handle, nil
 }
 
-func (h *Handle) do(ctx *core.Context, action chromedp.Action) error {
-	done := make(chan struct{})
-
-	go func() {
-		select {
-		case <-done:
-		case <-time.After(DEFAULT_SINGLE_ACTION_TIMEOUT):
-			h.close()
-		}
-	}()
-
-	defer func() {
-		done <- struct{}{}
-	}()
-
-	return chromedp.Run(h.chromedpContext,
-		action,
-	)
-}
-
 func (h *Handle) Nav(ctx *core.Context, u core.URL) error {
-	action := chromedp.Navigate(string(u))
-	return h.do(ctx, action)
+	return h.doNavigate(ctx, u)
 }
 
 func (h *Handle) WaitVisible(ctx *core.Context, s core.Str) error {
-	action := chromedp.WaitVisible(string(s))
-	return h.do(ctx, action)
+	return h.doWaitVisible(ctx, s)
 }
 
 func (h *Handle) Click(ctx *core.Context, s core.Str) error {
-	action := chromedp.Click(string(s))
-	return h.do(ctx, action)
+	return h.doClick(ctx, s)
 }
 
 func (h *Handle) ScreenshotPage(ctx *core.Context) (*core.ByteSlice, error) {
-	var b []byte
-
-	action := chromedp.CaptureScreenshot(&b)
-	if err := h.do(ctx, action); err != nil {
-		return nil, err
-	}
-
-	return &core.ByteSlice{Bytes: b, IsDataMutable: true}, nil
+	return h.doScreensotPage(ctx)
 }
 
 func (h *Handle) Screenshot(ctx *core.Context, sel core.Str) (*core.ByteSlice, error) {
-	var b []byte
-
-	action := chromedp.Screenshot(sel, &b)
-	if err := h.do(ctx, action); err != nil {
-		return nil, err
-	}
-
-	return &core.ByteSlice{Bytes: b, IsDataMutable: true}, nil
+	return h.doScreenshot(ctx, sel)
 }
 
 func (h *Handle) HtmlNode(ctx *core.Context, sel core.Str) (*_html.HTMLNode, error) {
-	var htmlS string
-
-	action := chromedp.OuterHTML(sel, &htmlS)
-	if err := h.do(ctx, action); err != nil {
-		return nil, err
-	}
-
-	return _html.ParseSingleNodeHTML(htmlS)
+	return h.doGetHTMLNode(ctx, sel)
 }
 
 func (h *Handle) Close(ctx *core.Context) {
