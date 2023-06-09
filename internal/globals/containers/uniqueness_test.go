d0ea756ffde7c850127a8af61849bf925161d69c
GraphR00t <113632189+GraphR00t@users.noreply.github.com>
2023-06-09 16:37:15 +0200

    containers: tests on uniq. constraint comparison

---
+++ internal/globals/containers/uniqueness_test.go
@@ -0,0 +1,93 @@
+package containers
+
+import (
+	"testing"
+
+	"github.com/inoxlang/inox/internal/core"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUniquenessConstraint(t *testing.T) {
+
+	t.Run("", func(t *testing.T) {
+		assert.True(t,
+			(UniquenessConstraint{
+				Type:         UniquePropertyValue,
+				PropertyName: core.PropertyName("a"),
+			}).Equal(UniquenessConstraint{
+				Type:         UniquePropertyValue,
+				PropertyName: core.PropertyName("a"),
+			}),
+		)
+
+		assert.True(t,
+			(UniquenessConstraint{
+				Type: UniqueURL,
+			}).Equal(UniquenessConstraint{
+				Type: UniqueURL,
+			}),
+		)
+
+		assert.True(t,
+			(UniquenessConstraint{
+				Type: UniqueRepr,
+			}).Equal(UniquenessConstraint{
+				Type: UniqueRepr,
+			}),
+		)
+
+		assert.False(t,
+			(UniquenessConstraint{
+				Type: UniqueURL,
+			}).Equal(UniquenessConstraint{
+				Type:         UniquePropertyValue,
+				PropertyName: core.PropertyName("a"),
+			}),
+		)
+
+		assert.False(t,
+			(UniquenessConstraint{
+				Type:         UniquePropertyValue,
+				PropertyName: core.PropertyName("a"),
+			}).Equal(UniquenessConstraint{
+				Type: UniqueURL,
+			}),
+		)
+
+		assert.False(t,
+			(UniquenessConstraint{
+				Type: UniqueURL,
+			}).Equal(UniquenessConstraint{
+				Type: UniqueRepr,
+			}),
+		)
+
+		assert.False(t,
+			(UniquenessConstraint{
+				Type: UniqueRepr,
+			}).Equal(UniquenessConstraint{
+				Type: UniqueURL,
+			}),
+		)
+
+		assert.False(t,
+			(UniquenessConstraint{
+				Type:         UniquePropertyValue,
+				PropertyName: core.PropertyName("a"),
+			}).Equal(UniquenessConstraint{
+				Type:         UniquePropertyValue,
+				PropertyName: core.PropertyName("b"),
+			}),
+		)
+
+		assert.False(t,
+			(UniquenessConstraint{
+				Type:         UniquePropertyValue,
+				PropertyName: core.PropertyName("b"),
+			}).Equal(UniquenessConstraint{
+				Type:         UniquePropertyValue,
+				PropertyName: core.PropertyName("a"),
+			}),
+		)
+	})
+}
