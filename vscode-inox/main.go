db70e612879cb5debbc7d8c6ef73857fc47891a1
GraphR00t <113632189+GraphR00t@users.noreply.github.com>
2023-06-10 16:04:22 +0200

    vscode-inox: provide inox FS + fixes

--- vscode-inox/main.go
+++ vscode-inox/main.go
@@ -3,12 +3,16 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"syscall/js"
 	"time"
 
 	"github.com/inoxlang/inox/internal/core"
 	_ "github.com/inoxlang/inox/internal/globals"
+	"github.com/inoxlang/inox/internal/globals/fs_ns"
+	"github.com/inoxlang/inox/internal/lsp/jsonrpc"
+	"github.com/rs/zerolog"
 
 	lsp "github.com/inoxlang/inox/internal/lsp"
 	"github.com/inoxlang/inox/internal/utils"
@@ -24,7 +28,19 @@ var printDebug *js.Value
 
 func main() {
 	ctx := core.NewContext(core.ContextConfig{})
+	state := core.NewGlobalState(ctx)
+	state.Out = utils.FnWriter{
+		WriteFn: func(p []byte) (n int, err error) {
+			if printDebug != nil {
+				printDebug.Invoke(OUT_PREFIX, string(p))
+			}
+			return len(p), nil
+		},
+	}
+	state.Logger = zerolog.New(state.Out)
+
 	pauseChan := make(chan struct{})
+	setupDoneChan := make(chan struct{}, 1)
 
 	lspInput := core.NewRingBuffer(ctx, LSP_INPUT_BUFFER_SIZE)
 	lspInputWriter := utils.FnReaderWriter{
@@ -59,10 +75,17 @@ func main() {
 	}
 
 	lspOuput := core.NewRingBuffer(ctx, LSP_OUTPUT_BUFFER_SIZE)
-	registerCallbacks(lspInputWriter, lspOuput)
+	registerCallbacks(lspInputWriter, lspOuput, setupDoneChan)
+
+	fmt.Println(OUT_PREFIX, "wait for setup() to be called by JS")
+	<-setupDoneChan
+	close(setupDoneChan)
 
 	fmt.Println(OUT_PREFIX, "start LSP server")
-	go lsp.StartLSPServer(ctx, lsp.LSPServerOptions{
+
+	serverCtx := ctx.BoundChild()
+	go lsp.StartLSPServer(serverCtx, lsp.LSPServerOptions{
+		InoxFS: true,
 		InternalStdio: &lsp.InternalStdio{
 			StdioInput:  lspInputWriter,
 			StdioOutput: lspOuput,
@@ -75,6 +98,30 @@ func main() {
 				},
 			},
 		},
+		OnSession: func(rpcCtx *core.Context, s *jsonrpc.Session) error {
+			printDebug.Invoke("new LSP session")
+
+			mainFs := fs_ns.NewMemFilesystem(lsp.DEFAULT_MAX_IN_MEM_FS_STORAGE_SIZE)
+			fls := lsp.NewFilesystem(mainFs, nil)
+
+			file := utils.Must(fls.Create("/main.ix"))
+			utils.Must(file.Write([]byte("manifest {\n\n}")))
+			utils.PanicIfErr(file.Close())
+
+			sessionCtx := core.NewContext(core.ContextConfig{
+				Permissions:          rpcCtx.GetGrantedPermissions(),
+				ForbiddenPermissions: rpcCtx.GetForbiddenPermissions(),
+
+				ParentContext: rpcCtx,
+				Filesystem:    fls,
+			})
+			tempState := core.NewGlobalState(sessionCtx)
+			tempState.Logger = state.Logger
+			tempState.Out = state.Out
+			s.SetContextOnce(sessionCtx)
+
+			return nil
+		},
 	})
 
 	fmt.Println(OUT_PREFIX, "end of main() reached: block with channel")
@@ -83,7 +130,7 @@ func main() {
 	<-channel
 }
 
-func registerCallbacks(lspInput io.ReadWriter, lspOutput *core.RingBuffer) {
+func registerCallbacks(lspInput io.ReadWriter, lspOutput *core.RingBuffer, setupDoneChan chan struct{}) {
 	exports := js.Global().Get("exports")
 
 	exports.Set("write_lsp_input", js.FuncOf(func(this js.Value, args []js.Value) any {
@@ -110,8 +157,11 @@ func registerCallbacks(lspInput io.ReadWriter, lspOutput *core.RingBuffer) {
 	}))
 
 	exports.Set("setup", js.FuncOf(func(this js.Value, args []js.Value) any {
+		fmt.Println(OUT_PREFIX, "setup() called by JS")
+
 		if printDebug != nil {
-			printDebug.Invoke(OUT_PREFIX, "setup() called again by JS")
+			fmt.Println("setup() already called by JS !")
+			return js.ValueOf("")
 		}
 
 		IWD := args[0].Get(core.INITIAL_WORKING_DIR_VARNAME).String()
@@ -123,6 +173,9 @@ func registerCallbacks(lspInput io.ReadWriter, lspOutput *core.RingBuffer) {
 		})
 
 		b := lspOutput.ReadableBytesCopy()
+
+		setupDoneChan <- struct{}{}
+
 		return js.ValueOf(string(b))
 	}))
 
