8b34d251ba4d17568522dea30a5ed5be424c2f8c
GraphR00t <113632189+GraphR00t@users.noreply.github.com>
2023-07-06 13:55:33 +0200

    core: add loadInstanceFnRegistry

---
+++ internal/core/load.go
@@ -0,0 +1,47 @@
+package core
+
+import (
+	"errors"
+	"reflect"
+	"sync"
+)
+
+var (
+	loadInstanceFnregistry     = map[reflect.Type] /* pattern type*/ LoadInstanceFn{}
+	loadInstanceFnRegistryLock sync.Mutex
+
+	ErrNonUniqueLoadInstanceFnRegistration = errors.New("non unique load instance function registration")
+	ErrLoadingRequireTransaction           = errors.New("loading a value requires a transaction")
+	ErrTransactionsNotSupportedYet         = errors.New("transactions not supported yet")
+	ErrFailedToLoadNonExistingValue        = errors.New("failed to non-existing value")
+)
+
+type SerializedValueStorage interface {
+	GetSerialized(ctx *Context, key Path) (string, bool)
+	Has(ctx *Context, key Path) bool
+	SetSerialized(ctx *Context, key Path, serialized string)
+	InsertSerialized(ctx *Context, key Path, serialized string)
+}
+
+type LoadInstanceFn func(ctx *Context, key Path, storage SerializedValueStorage, pattern Pattern) (Value, error)
+
+func RegisterLoadInstanceFn(patternType reflect.Type, fn LoadInstanceFn) {
+	loadInstanceFnRegistryLock.Lock()
+	defer loadInstanceFnRegistryLock.Unlock()
+
+	_, ok := loadInstanceFnregistry[patternType]
+	if ok {
+		panic(ErrNonUniqueLoadInstanceFnRegistration)
+	}
+
+	loadInstanceFnregistry[patternType] = fn
+}
+
+func getLoadInstanceFn(typ reflect.Type) (LoadInstanceFn, bool) {
+	loadInstanceFnRegistryLock.Lock()
+	defer loadInstanceFnRegistryLock.Unlock()
+
+	fn, ok := loadInstanceFnregistry[typ]
+
+	return fn, ok
+}
