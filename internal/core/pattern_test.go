4df4447a81fe595fd6628fb9c33448879dfb60b3
GraphR00t <113632189+GraphR00t@users.noreply.github.com>
2023-09-21 11:42:05 +0200

    patterns: improve tests on IntPattern

--- internal/core/pattern_test.go
+++ internal/core/pattern_test.go
@@ -146,14 +146,15 @@ func TestIntRangePattern(t *testing.T) {
 
 	t.Run("0..100", func(t *testing.T) {
 		patt := NewIncludedEndIntRangePattern(0, 100, -1)
-
 		assert.True(t, patt.Test(ctx, Int(0)))
+		assert.True(t, patt.Test(ctx, Int(1)))
+		assert.True(t, patt.Test(ctx, Int(2)))
 		assert.True(t, patt.Test(ctx, Int(3)))
+		assert.True(t, patt.Test(ctx, Int(4)))
 		assert.True(t, patt.Test(ctx, Int(6)))
 		assert.True(t, patt.Test(ctx, Int(9)))
 		assert.True(t, patt.Test(ctx, Int(99)))
 
-		assert.False(t, patt.Test(ctx, Int(-1)))
 		assert.False(t, patt.Test(ctx, Int(102)))
 	})
 
@@ -166,6 +167,9 @@ func TestIntRangePattern(t *testing.T) {
 		assert.True(t, patt.Test(ctx, Int(99)))
 
 		assert.False(t, patt.Test(ctx, Int(-1)))
+		assert.False(t, patt.Test(ctx, Int(1)))
+		assert.False(t, patt.Test(ctx, Int(2)))
+		assert.False(t, patt.Test(ctx, Int(4)))
 		assert.False(t, patt.Test(ctx, Int(102)))
 	})
 }
