e850e8ed929af9853b12d3c44918d8d8cd5e4b9d
GraphR00t <113632189+GraphR00t@users.noreply.github.com>
2023-10-21 01:17:51 +0200

    in_mem_ds: impl DirectedGraph.LongestPath()

--- internal/in_mem_ds/directed_graph_path_test.go
+++ internal/in_mem_ds/directed_graph_path_test.go
@@ -6,17 +6,29 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDirectedGraphLongestPathLen(t *testing.T) {
+func TestDirectedGraphLongestPath(t *testing.T) {
 
 	t.Run("empty graph", func(t *testing.T) {
 		g := NewDirectedGraph[string, int](ThreadUnsafe)
-		assert.Equal(t, 0, g.LongestPathLen())
+		if !assert.Equal(t, 0, g.LongestPathLen()) {
+			return
+		}
+
+		path, len := g.LongestPath()
+		assert.Equal(t, 0, len)
+		assert.Empty(t, 0, path)
 	})
 
 	t.Run("single node, no edges", func(t *testing.T) {
 		g := NewDirectedGraph[string, int](ThreadUnsafe)
 		g.AddNode("A")
-		assert.Equal(t, 0, g.LongestPathLen())
+		if !assert.Equal(t, 0, g.LongestPathLen()) {
+			return
+		}
+
+		path, len := g.LongestPath()
+		assert.Equal(t, 0, len)
+		assert.Empty(t, 0, path)
 	})
 
 	t.Run("two nodes, no edges", func(t *testing.T) {
@@ -24,7 +36,13 @@ func TestDirectedGraphLongestPathLen(t *testing.T) {
 		g.AddNode("A")
 		g.AddNode("B")
 
-		assert.Equal(t, 0, g.LongestPathLen())
+		if !assert.Equal(t, 0, g.LongestPathLen()) {
+			return
+		}
+
+		path, len := g.LongestPath()
+		assert.Equal(t, 0, len)
+		assert.Empty(t, 0, path)
 	})
 
 	t.Run("two nodes, A -> B", func(t *testing.T) {
@@ -33,17 +51,63 @@ func TestDirectedGraphLongestPathLen(t *testing.T) {
 		B := g.AddNode("B")
 		g.SetEdge(A, B, -1)
 
-		assert.Equal(t, 1, g.LongestPathLen())
+		if !assert.Equal(t, 1, g.LongestPathLen()) {
+			return
+		}
+
+		path, len := g.LongestPath()
+		assert.Equal(t, 1, len)
+		assert.Equal(t, []NodeId{A, B}, path)
 	})
 
-	t.Run("two nodes, A -> B -> A", func(t *testing.T) {
+	t.Run("two nodes, A -> B -> A (cycle)", func(t *testing.T) {
 		g := NewDirectedGraph[string, int](ThreadUnsafe)
 		A := g.AddNode("A")
 		B := g.AddNode("B")
 		g.SetEdge(A, B, -1)
 		g.SetEdge(B, A, -1)
 
-		assert.Equal(t, -1, g.LongestPathLen())
+		if !assert.Equal(t, -1, g.LongestPathLen()) {
+			return
+		}
+
+		path, len := g.LongestPath()
+		assert.Equal(t, -1, len)
+		assert.Empty(t, path)
+	})
+
+	t.Run("two nodes, A and B, the C node was added and directly removed", func(t *testing.T) {
+		g := NewDirectedGraph[string, int](ThreadUnsafe)
+		g.AddNode("A")
+		g.AddNode("B")
+		C := g.AddNode("C")
+		g.RemoveNode(C)
+
+		if !assert.Equal(t, 0, g.LongestPathLen()) {
+			return
+		}
+
+		path, len := g.LongestPath()
+		assert.Equal(t, 0, len)
+		assert.Empty(t, path)
+	})
+
+	t.Run("two nodes, A -> C, the node B was removed and then node C was added", func(t *testing.T) {
+		g := NewDirectedGraph[string, int](ThreadUnsafe)
+		A := g.AddNode("A")
+		B := g.AddNode("B")
+		C := g.AddNode("C")
+		g.RemoveNode(B)
+
+		g.SetEdge(A, C, -1)
+
+		if !assert.Equal(t, 1, g.LongestPathLen()) {
+			return
+		}
+
+		path, len := g.LongestPath()
+		assert.Equal(t, 1, len)
+		assert.Equal(t, []NodeId{A, C}, path)
 	})
 
 	t.Run("three nodes, no edges", func(t *testing.T) {
@@ -52,7 +116,45 @@ func TestDirectedGraphLongestPathLen(t *testing.T) {
 		g.AddNode("B")
 		g.AddNode("C")
 
-		assert.Equal(t, 0, g.LongestPathLen())
+		if !assert.Equal(t, 0, g.LongestPathLen()) {
+			return
+		}
+
+		path, len := g.LongestPath()
+		assert.Equal(t, 0, len)
+		assert.Empty(t, path)
+	})
+
+	t.Run("three nodes, A -> B", func(t *testing.T) {
+		g := NewDirectedGraph[string, int](ThreadUnsafe)
+		A := g.AddNode("A")
+		B := g.AddNode("B")
+		_ = g.AddNode("C")
+		g.SetEdge(A, B, -1)
+
+		if !assert.Equal(t, 1, g.LongestPathLen()) {
+			return
+		}
+
+		path, len := g.LongestPath()
+		assert.Equal(t, 1, len)
+		assert.Equal(t, []NodeId{A, B}, path)
+	})
+
+	t.Run("three nodes, A -> C", func(t *testing.T) {
+		g := NewDirectedGraph[string, int](ThreadUnsafe)
+		A := g.AddNode("A")
+		_ = g.AddNode("B")
+		C := g.AddNode("C")
+		g.SetEdge(A, C, -1)
+
+		if !assert.Equal(t, 1, g.LongestPathLen()) {
+			return
+		}
+
+		path, len := g.LongestPath()
+		assert.Equal(t, 1, len)
+		assert.Equal(t, []NodeId{A, C}, path)
 	})
 
 	t.Run("three nodes, A -> B -> C", func(t *testing.T) {
@@ -63,7 +165,13 @@ func TestDirectedGraphLongestPathLen(t *testing.T) {
 		g.SetEdge(A, B, -1)
 		g.SetEdge(B, C, -1)
 
-		assert.Equal(t, 2, g.LongestPathLen())
+		if !assert.Equal(t, 2, g.LongestPathLen()) {
+			return
+		}
+
+		path, len := g.LongestPath()
+		assert.Equal(t, 2, len)
+		assert.Equal(t, []NodeId{A, B, C}, path)
 	})
 
 	t.Run("three nodes, A -> B -> C -> A (cycle)", func(t *testing.T) {
@@ -75,7 +183,13 @@ func TestDirectedGraphLongestPathLen(t *testing.T) {
 		g.SetEdge(B, C, -1)
 		g.SetEdge(C, A, -1)
 
-		assert.Equal(t, -1, g.LongestPathLen())
+		if !assert.Equal(t, -1, g.LongestPathLen()) {
+			return
+		}
+
+		path, len := g.LongestPath()
+		assert.Equal(t, -1, len)
+		assert.Empty(t, path)
 	})
 
 	t.Run("three nodes, A -> B -> C -> B (cycle)", func(t *testing.T) {
@@ -87,7 +201,13 @@ func TestDirectedGraphLongestPathLen(t *testing.T) {
 		g.SetEdge(B, C, -1)
 		g.SetEdge(C, B, -1)
 
-		assert.Equal(t, -1, g.LongestPathLen())
+		if !assert.Equal(t, -1, g.LongestPathLen()) {
+			return
+		}
+
+		path, len := g.LongestPath()
+		assert.Equal(t, -1, len)
+		assert.Empty(t, path)
 	})
 
 	t.Run("three nodes, A -> (B & C)", func(t *testing.T) {
@@ -98,7 +218,17 @@ func TestDirectedGraphLongestPathLen(t *testing.T) {
 		g.SetEdge(A, B, -1)
 		g.SetEdge(A, C, -1)
 
-		assert.Equal(t, 1, g.LongestPathLen())
+		if !assert.Equal(t, 1, g.LongestPathLen()) {
+			return
+		}
+
+		path, len := g.LongestPath()
+		assert.Equal(t, 1, len)
+		if path[1] == B {
+			assert.Equal(t, []NodeId{A, B}, path)
+		} else {
+			assert.Equal(t, []NodeId{A, C}, path)
+		}
 	})
 
 	t.Run("three nodes, A -> (B & C), B -> C", func(t *testing.T) {
@@ -111,7 +241,13 @@ func TestDirectedGraphLongestPathLen(t *testing.T) {
 		g.SetEdge(A, C, -1)
 
 		//A -> B -> C
-		assert.Equal(t, 2, g.LongestPathLen())
+		if !assert.Equal(t, 2, g.LongestPathLen()) {
+			return
+		}
+
+		path, len := g.LongestPath()
+		assert.Equal(t, 2, len)
+		assert.Equal(t, []NodeId{A, B, C}, path)
 	})
 
 	t.Run("three nodes, A -> (B & C), B -> C, C -> A (cycle)", func(t *testing.T) {
@@ -124,7 +260,13 @@ func TestDirectedGraphLongestPathLen(t *testing.T) {
 		g.SetEdge(A, C, -1)
 		g.SetEdge(C, A, -1)
 
-		assert.Equal(t, -1, g.LongestPathLen())
+		if !assert.Equal(t, -1, g.LongestPathLen()) {
+			return
+		}
+
+		path, len := g.LongestPath()
+		assert.Equal(t, -1, len)
+		assert.Empty(t, path)
 	})
 
 	t.Run("three nodes, A -> (B & C), C -> A (cycle)", func(t *testing.T) {
@@ -136,7 +278,13 @@ func TestDirectedGraphLongestPathLen(t *testing.T) {
 		g.SetEdge(A, C, -1)
 		g.SetEdge(C, A, -1)
 
-		assert.Equal(t, -1, g.LongestPathLen())
+		if !assert.Equal(t, -1, g.LongestPathLen()) {
+			return
+		}
+
+		path, len := g.LongestPath()
+		assert.Equal(t, -1, len)
+		assert.Empty(t, path)
 	})
 
 	t.Run("three nodes, A -> (B & C), B -> C, C -> B (cycle)", func(t *testing.T) {
@@ -149,7 +297,13 @@ func TestDirectedGraphLongestPathLen(t *testing.T) {
 		g.SetEdge(B, C, -1)
 		g.SetEdge(C, B, -1)
 
-		assert.Equal(t, -1, g.LongestPathLen())
+		if !assert.Equal(t, -1, g.LongestPathLen()) {
+			return
+		}
+
+		path, len := g.LongestPath()
+		assert.Equal(t, -1, len)
+		assert.Empty(t, path)
 	})
 
 	t.Run("three nodes, A -> (B & C), B -> C, C -> (B & A) (cycle)", func(t *testing.T) {
@@ -163,7 +317,76 @@ func TestDirectedGraphLongestPathLen(t *testing.T) {
 		g.SetEdge(C, B, -1)
 		g.SetEdge(C, A, -1)
 
-		assert.Equal(t, -1, g.LongestPathLen())
+		if !assert.Equal(t, -1, g.LongestPathLen()) {
+			return
+		}
+
+		path, len := g.LongestPath()
+		assert.Equal(t, -1, len)
+		assert.Empty(t, path)
+	})
+
+	t.Run("four nodes, A -> B -> C -> D", func(t *testing.T) {
+		g := NewDirectedGraph[string, int](ThreadUnsafe)
+		A := g.AddNode("A")
+		B := g.AddNode("B")
+		C := g.AddNode("C")
+		D := g.AddNode("D")
+
+		g.SetEdge(A, B, -1)
+		g.SetEdge(B, C, -1)
+		g.SetEdge(C, D, -1)
+
+		if !assert.Equal(t, 3, g.LongestPathLen()) {
+			return
+		}
+
+		path, len := g.LongestPath()
+		assert.Equal(t, 3, len)
+		assert.Equal(t, []NodeId{A, B, C, D}, path)
+	})
+
+	t.Run("four nodes, A -> B  C -> D", func(t *testing.T) {
+		g := NewDirectedGraph[string, int](ThreadUnsafe)
+		A := g.AddNode("A")
+		B := g.AddNode("B")
+		C := g.AddNode("C")
+		D := g.AddNode("D")
+
+		g.SetEdge(A, B, -1)
+		g.SetEdge(C, D, -1)
+
+		if !assert.Equal(t, 1, g.LongestPathLen()) {
+			return
+		}
+
+		path, len := g.LongestPath()
+		assert.Equal(t, 1, len)
+		if path[0] == A {
+			assert.Equal(t, []NodeId{A, B}, path)
+		} else {
+			assert.Equal(t, []NodeId{C, D}, path)
+		}
+	})
+
+	t.Run("four nodes, A -> B -> C", func(t *testing.T) {
+		g := NewDirectedGraph[string, int](ThreadUnsafe)
+		A := g.AddNode("A")
+		B := g.AddNode("B")
+		C := g.AddNode("C")
+		_ = g.AddNode("D")
+
+		g.SetEdge(A, B, -1)
+		g.SetEdge(B, C, -1)
+
+		//A -> B -> C
+		if !assert.Equal(t, 2, g.LongestPathLen()) {
+			return
+		}
+
+		path, len := g.LongestPath()
+		assert.Equal(t, 2, len)
+		assert.Equal(t, []NodeId{A, B, C}, path)
 	})
 
 	t.Run("four nodes, A -> (B & C), B -> D, C -> D, D -> A (cycle)", func(t *testing.T) {
@@ -179,7 +402,13 @@ func TestDirectedGraphLongestPathLen(t *testing.T) {
 		g.SetEdge(C, D, -1)
 		g.SetEdge(D, A, -1)
 
-		assert.Equal(t, -1, g.LongestPathLen())
+		if !assert.Equal(t, -1, g.LongestPathLen()) {
+			return
+		}
+
+		path, len := g.LongestPath()
+		assert.Equal(t, -1, len)
+		assert.Empty(t, path)
 	})
 
 	t.Run("four nodes, A -> (B & C), C -> D, D -> A (cycle)", func(t *testing.T) {
@@ -194,7 +423,13 @@ func TestDirectedGraphLongestPathLen(t *testing.T) {
 		g.SetEdge(C, D, -1)
 		g.SetEdge(D, A, -1)
 
-		assert.Equal(t, -1, g.LongestPathLen())
+		if !assert.Equal(t, -1, g.LongestPathLen()) {
+			return
+		}
+
+		path, len := g.LongestPath()
+		assert.Equal(t, -1, len)
+		assert.Empty(t, path)
 	})
 
 	t.Run("five nodes, A -> (B & C), C -> D, B -> E", func(t *testing.T) {
@@ -210,8 +445,18 @@ func TestDirectedGraphLongestPathLen(t *testing.T) {
 		g.SetEdge(C, D, -1)
 		g.SetEdge(B, E, -1)
 
-		//A -> C -> E or A -> B -> E
-		assert.Equal(t, 2, g.LongestPathLen())
+		//A -> C -> D or A -> B -> E
+		if !assert.Equal(t, 2, g.LongestPathLen()) {
+			return
+		}
+
+		path, len := g.LongestPath()
+		assert.Equal(t, 2, len)
+		if path[1] == B {
+			assert.Equal(t, []NodeId{A, B, E}, path)
+		} else {
+			assert.Equal(t, []NodeId{A, C, D}, path)
+		}
 	})
 
 	t.Run("six nodes, A -> (B & C), C -> D, D -> E, B -> F", func(t *testing.T) {
@@ -230,6 +475,12 @@ func TestDirectedGraphLongestPathLen(t *testing.T) {
 		g.SetEdge(B, F, -1)
 
 		//A -> C -> D -> E
-		assert.Equal(t, 3, g.LongestPathLen())
+		if !assert.Equal(t, 3, g.LongestPathLen()) {
+			return
+		}
+
+		path, len := g.LongestPath()
+		assert.Equal(t, 3, len)
+		assert.Equal(t, []NodeId{A, C, D, E}, path)
 	})
 }
