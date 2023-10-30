fad6a9d60c66370c024b13e637f7ff108fa5f5de
GraphR00t <113632189+GraphR00t@users.noreply.github.com>
2023-10-30 23:37:03 +0100

    fs: update interfaces StatCapable and SyncCapable

--- internal/afs/file.go
+++ internal/afs/file.go
@@ -3,6 +3,8 @@ package afs
 import (
 	"errors"
 	"os"
+
+	"github.com/go-git/go-billy/v5"
 )
 
 var (
@@ -10,9 +12,11 @@ var (
 )
 
 type StatCapable interface {
+	billy.File
 	Stat() (os.FileInfo, error)
 }
 
 type SyncCapable interface {
+	billy.File
 	Sync() error
 }
