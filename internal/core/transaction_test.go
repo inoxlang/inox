e0a697eb2db09218f2289df1f41335d2d6c8f8d0
GraphR00t <113632189+GraphR00t@users.noreply.github.com>
2023-11-08 17:08:39 +0100

    core: update a memory assertion in a test

--- internal/core/transaction_test.go
+++ internal/core/transaction_test.go
@@ -15,7 +15,7 @@ func TestTransaction(t *testing.T) {
 		startMemStats := new(runtime.MemStats)
 		runtime.ReadMemStats(startMemStats)
 
-		defer utils.AssertNoMemoryLeak(t, startMemStats, 10, utils.AssertNoMemoryLeakOptions{
+		defer utils.AssertNoMemoryLeak(t, startMemStats, 100, utils.AssertNoMemoryLeakOptions{
 			PreSleepDurationMillis: 100,
 		})
 	}
