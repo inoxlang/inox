525269293856cfdc134ef88cd271a2001dba5b2e
GraphR00t <113632189+GraphR00t@users.noreply.github.com>
2023-11-16 15:11:58 +0100

    utils: rework random float generation

--- internal/utils/rand_test.go
+++ internal/utils/rand_test.go
@@ -1,176 +1,48 @@
-// source:
-// https://github.com/shogo82148/random-float/tree/b8df8274c0fd35cfd34e632acd62e465b3b25c77
-// by Ichinose Shogo, MIT Licensed.
-
 package utils
 
 import (
+	"crypto/rand"
 	"math"
-	"math/rand"
+	"math/big"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-// zeroSource is a rand.Source that always returns 0.
-type zeroSource struct{}
+func TestRandFloat(t *testing.T) {
 
-func (zeroSource) Seed(int64)   {}
-func (zeroSource) Int63() int64 { return 0 }
+	t.Run("-math.MaxFloat64 .. math.MaxFloat64", func(t *testing.T) {
+		//check infinity is never returned
+		for i := 0; i < 1_000_000; i++ {
+			seed := Must(rand.Int(rand.Reader, big.NewInt(math.MaxInt64))).Uint64()
 
-// zeroSource64 is a rand.Source64 that always returns 0.
-type zeroSource64 struct {
-	zeroSource
-}
-
-func (zeroSource64) Uint64() uint64 { return 0 }
-
-// oneSource is a rand.Source that always returns all 1s.
-type oneSource struct{}
-
-func (oneSource) Seed(int64)   {}
-func (oneSource) Int63() int64 { return 1<<63 - 1 }
-
-// oneSource64 is a rand.Source64 that always returns all 1s.
-type oneSource64 struct {
-	oneSource
-}
-
-func (oneSource64) Uint64() uint64 { return 1<<64 - 1 }
-
-func TestFloat32(t *testing.T) {
-	if testing.Short() {
-		t.SkipNow()
-	}
-
-	r := NewFloatRand(rand.NewSource(42))
-	for i := 0; i < 1e8; i++ {
-		f := r.Float32()
-		if f < 0 || f >= 1 {
-			t.Errorf("invalid range: %x", f)
+			result := RandFloat(-math.MaxFloat64, math.MaxFloat64, seed)
+			if !assert.False(t, math.IsInf(result, 0)) {
+				return
+			}
 		}
-	}
-}
+	})
 
-func TestFloat32Zero(t *testing.T) {
-	var r *FloatRand
-	var f float32
+	t.Run("-0.5*math.MaxFloat64 .. 0.5*math.MaxFloat64", func(t *testing.T) {
+		lower := -math.MaxInt64 * 0.5
+		upper := math.MaxInt64 * 0.5
 
-	r = NewFloatRand(zeroSource{})
-	f = r.Float32()
-	if f != 0 {
-		t.Errorf("invalid range: %x", f)
-	}
+		//check returned value is not ouf of bounds
+		for i := 0; i < 1_000_000; i++ {
+			seed := Must(rand.Int(rand.Reader, big.NewInt(math.MaxInt64))).Uint64()
 
-	r = NewFloatRand(zeroSource64{})
-	f = r.Float32()
-	if f != 0 {
-		t.Errorf("invalid range: %x", f)
-	}
-}
+			result := RandFloat(lower, upper, seed)
+			if !assert.False(t, math.IsInf(result, 0)) {
+				return
+			}
 
-func TestFloat32One(t *testing.T) {
-	var r *FloatRand
-	var f float32
-
-	r = NewFloatRand(oneSource{})
-	f = r.Float32()
-	if f != math.Nextafter32(1, 0) {
-		t.Errorf("invalid range: %x", f)
-	}
-
-	r = NewFloatRand(oneSource64{})
-	f = r.Float32()
-	if f != math.Nextafter32(1, 0) {
-		t.Errorf("invalid range: %x", f)
-	}
-}
-
-func TestFloat64(t *testing.T) {
-	if testing.Short() {
-		t.SkipNow()
-	}
-
-	r := NewFloatRand(rand.NewSource(42))
-	for i := 0; i < 1e8; i++ {
-		f := r.Float64()
-		if f < 0 || f >= 1 {
-			t.Errorf("invalid range: %x", f)
+			if !assert.True(t, result >= lower) {
+				return
+			}
+			if !assert.True(t, result <= upper) {
+				return
+			}
 		}
-	}
-}
+	})
 
-func TestFloat64Zero(t *testing.T) {
-	var r *FloatRand
-	var f float64
-
-	r = NewFloatRand(zeroSource{})
-	f = r.Float64()
-	if f != 0 {
-		t.Errorf("invalid range: %x", f)
-	}
-
-	r = NewFloatRand(zeroSource64{})
-	f = r.Float64()
-	if f != 0 {
-		t.Errorf("invalid range: %x", f)
-	}
-}
-
-func TestFloat64One(t *testing.T) {
-	var r *FloatRand
-	var f float64
-
-	r = NewFloatRand(oneSource{})
-	f = r.Float64()
-	if f != math.Nextafter(1, 0) {
-		t.Errorf("invalid range: %x", f)
-	}
-
-	r = NewFloatRand(oneSource64{})
-	f = r.Float64()
-	if f != math.Nextafter(1, 0) {
-		t.Errorf("invalid range: %x", f)
-	}
-}
-
-func BenchmarkFloat32(b *testing.B) {
-	r := NewFloatRand(rand.NewSource(42))
-	for i := 0; i < b.N; i++ {
-		r.Float32()
-	}
-}
-
-func BenchmarkFloat64(b *testing.B) {
-	r := NewFloatRand(rand.NewSource(42))
-	for i := 0; i < b.N; i++ {
-		r.Float64()
-	}
-}
-
-func BenchmarkRandFloat32(b *testing.B) {
-	r := rand.New(rand.NewSource(42))
-	for i := 0; i < b.N; i++ {
-		r.Float32()
-	}
-}
-
-func BenchmarkRandFloat64(b *testing.B) {
-	r := rand.New(rand.NewSource(42))
-	for i := 0; i < b.N; i++ {
-		r.Float64()
-	}
-}
-
-func BenchmarkSource(b *testing.B) {
-	src := rand.NewSource(42)
-	for i := 0; i < b.N; i++ {
-		src.Int63()
-	}
-}
-
-func BenchmarkSource64(b *testing.B) {
-	src := rand.NewSource(42)
-	s64 := src.(rand.Source64)
-	for i := 0; i < b.N; i++ {
-		s64.Uint64()
-	}
 }
