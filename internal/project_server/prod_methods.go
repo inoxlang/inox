a309e12159acaa73341893ce6f684ffd0f542642
GraphR00t <113632189+GraphR00t@users.noreply.github.com>
2023-12-05 18:58:36 +0100

    lsp/prod: add method to stop an app

--- internal/project_server/prod_methods.go
+++ internal/project_server/prod_methods.go
@@ -9,6 +9,7 @@ import (
 )
 
 const DEPLOY_PROD_APP_METHOD = "prod/deployApplication"
+const STOP_PROD_APP_METHOD = "prod/stopApplication"
 
 type DeployAppParams struct {
 	AppName          string `json:"name"`
@@ -19,11 +20,18 @@ type DeployAppResponse struct {
 	Error string `json:"error,omitempty"`
 }
 
+type StopAppParams struct {
+	AppName string `json:"name"`
+}
+
+type StopAppResponse struct {
+	Error string `json:"error,omitempty"`
+}
+
 func registerProdMethodHandlers(server *lsp.Server, opts LSPServerConfiguration, projectRegistry *project.Registry) {
 
 	server.OnCustom(jsonrpc.MethodInfo{
-		Name:          DEPLOY_PROD_APP_METHOD,
-		SensitiveData: true,
+		Name: DEPLOY_PROD_APP_METHOD,
 		NewRequest: func() interface{} {
 			return &DeployAppParams{}
 		},
@@ -60,6 +68,38 @@ func registerProdMethodHandlers(server *lsp.Server, opts LSPServerConfiguration,
 			}
 
 			return DeployAppResponse{}, nil
+		},
+	})
+
+	server.OnCustom(jsonrpc.MethodInfo{
+		Name: STOP_PROD_APP_METHOD,
+		NewRequest: func() interface{} {
+			return &StopAppParams{}
+		},
+		RateLimits: []int{0, 3, 10},
+		Handler: func(ctx context.Context, req interface{}) (interface{}, error) {
+			session := jsonrpc.GetSession(ctx)
+			params := req.(*StopAppParams)
+
+			proj, ok := getProject(session)
+			if !ok {
+				return jsonrpc.ResponseError{
+					Code:    jsonrpc.InternalError.Code,
+					Message: "project is not open",
+				}, nil
+			}
+
+			err := proj.StopApplication(project.StopApplicationParams{
+				AppName: params.AppName,
+			})
+
+			if err != nil {
+				return StopAppResponse{
+					Error: err.Error(),
+				}, nil
+			}
+
+			return StopAppResponse{}, nil
 		},
 	})
 }
