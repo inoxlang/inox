5cbe3a743c61ccd247273a86c2c238657815c873
GraphR00t <113632189+GraphR00t@users.noreply.github.com>
2023-12-06 23:42:52 +0100

    compressarch: support untarring in memory

---
+++ internal/compressarch/untar.go
@@ -0,0 +1,30 @@
+package compressarch
+
+import (
+	"archive/tar"
+	"bytes"
+	"io"
+	"io/fs"
+)
+
+
+func UntarInMemory(tarball []byte, entryCallbackFunc func(info fs.FileInfo, reader io.Reader) error) error {
+	tarReader := tar.NewReader(bytes.NewReader(tarball))
+
+	for {
+		header, err := tarReader.Next()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+
+		info := header.FileInfo()
+
+		err = entryCallbackFunc(info, tarReader)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
