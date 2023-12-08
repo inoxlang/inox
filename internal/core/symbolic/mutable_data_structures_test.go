3348b939c2acb46136b0d86168631d1f034735c6
GraphR00t <113632189+GraphR00t@users.noreply.github.com>
2023-12-08 17:17:04 +0100

    symb: add tests on Array

--- internal/core/symbolic/mutable_data_structures_test.go
+++ internal/core/symbolic/mutable_data_structures_test.go
@@ -519,7 +519,168 @@ func TestList(t *testing.T) {
 	})
 }
 
-func TestSymbolicDictionary(t *testing.T) {
+func TestArray(t *testing.T) {
+
+	t.Run("Test()", func(t *testing.T) {
+		cases := []struct {
+			array1 *Array
+			array2 *Array
+			ok     bool
+		}{
+			{
+				&Array{elements: nil, generalElement: ANY_SERIALIZABLE},
+				&Array{elements: nil, generalElement: ANY_SERIALIZABLE},
+				true,
+			},
+			{
+				&Array{elements: nil, generalElement: ANY_SERIALIZABLE},
+				&Array{elements: nil, generalElement: ANY_INT},
+				true,
+			},
+			{
+				&Array{elements: nil, generalElement: ANY_INT},
+				&Array{elements: nil, generalElement: ANY_SERIALIZABLE},
+				false,
+			},
+			{
+				&Array{elements: nil, generalElement: ANY_INT},
+				&Array{elements: nil, generalElement: ANY_INT},
+				true,
+			},
+			{
+				&Array{elements: nil, generalElement: ANY_INT},
+				&Array{elements: []Value{ANY_INT}, generalElement: nil},
+				true,
+			},
+			{
+				&Array{elements: nil, generalElement: ANY_INT},
+				&Array{elements: []Value{ANY_INT, ANY_BOOL}, generalElement: nil},
+				false,
+			},
+			{
+				&Array{elements: []Value{ANY_INT}, generalElement: nil},
+				&Array{elements: nil, generalElement: ANY_INT},
+				false,
+			},
+			{
+				&Array{elements: []Value{ANY_INT}, generalElement: nil},
+				&Array{elements: []Value{ANY_INT}, generalElement: nil},
+				true,
+			},
+			{
+				&Array{elements: []Value{ANY_INT}, generalElement: nil},
+				&Array{elements: []Value{ANY_BOOL}, generalElement: nil},
+				false,
+			},
+			{
+				&Array{elements: []Value{ANY_INT, ANY_INT}, generalElement: nil},
+				&Array{elements: []Value{ANY_INT, ANY_BOOL}, generalElement: nil},
+				false,
+			},
+		}
+
+		for _, testCase := range cases {
+			t.Run(fmt.Sprint(testCase.array1, "_", testCase.array2), func(t *testing.T) {
+				assert.Equal(t, testCase.ok, testCase.array1.Test(testCase.array2, RecTestCallState{}))
+			})
+		}
+
+		t.Run("an infinite recursion should raise the error "+ErrMaximumSymbolicTestCallDepthReached.Error(), func(t *testing.T) {
+			array1 := &Array{}
+			array1.elements = []Value{array1}
+			assert.PanicsWithError(t, ErrMaximumSymbolicTestCallDepthReached.Error(), func() {
+				array1.Test(array1, RecTestCallState{})
+			})
+
+			array2 := &Array{}
+			array2.generalElement = array2
+			assert.PanicsWithError(t, ErrMaximumSymbolicTestCallDepthReached.Error(), func() {
+				array2.Test(array2, RecTestCallState{})
+			})
+		})
+	})
+
+	t.Run("element() and IteratorElementValue()", func(t *testing.T) {
+		cases := []struct {
+			array   *Array
+			element Value
+		}{
+			{
+				&Array{elements: nil, generalElement: ANY},
+				ANY,
+			},
+			{
+				&Array{elements: nil, generalElement: ANY_INT},
+				ANY_INT,
+			},
+			{
+				&Array{elements: []Value{ANY_INT}, generalElement: nil},
+				ANY_INT,
+			},
+			{
+				&Array{elements: []Value{ANY_INT, ANY_INT}, generalElement: nil},
+				ANY_INT,
+			},
+			{
+				&Array{elements: []Value{INT_1, INT_2}, generalElement: nil},
+				joinValues([]Value{INT_1, INT_2}),
+			},
+		}
+
+		for _, testCase := range cases {
+			t.Run(Stringify(testCase.array), func(t *testing.T) {
+				element := testCase.array.element()
+				if !assert.Equal(t, testCase.element, element) {
+					return
+				}
+				assert.Equal(t, testCase.element, testCase.array.IteratorElementValue())
+			})
+		}
+
+		t.Run("an infinite recursion should raise the error "+ErrMaximumSymbolicTestCallDepthReached.Error(), func(t *testing.T) {
+			array1 := &Array{}
+			array1.elements = []Value{array1}
+			assert.PanicsWithError(t, ErrMaximumSymbolicTestCallDepthReached.Error(), func() {
+				array1.Test(array1, RecTestCallState{})
+			})
+
+			array2 := &Array{}
+			array2.generalElement = array2
+			assert.PanicsWithError(t, ErrMaximumSymbolicTestCallDepthReached.Error(), func() {
+				array2.Test(array2, RecTestCallState{})
+			})
+		})
+	})
+
+	t.Run("Contains()", func(t *testing.T) {
+		intList := NewListOf(ANY_INT)
+		assertMayContainButNotCertain(t, intList, ANY_SERIALIZABLE)
+		assertMayContainButNotCertain(t, intList, ANY_INT)
+		assertMayContainButNotCertain(t, intList, INT_1)
+		assertMayContainButNotCertain(t, intList, INT_2)
+
+		listWithConcreteValue := NewListOf(INT_1)
+		assertContains(t, listWithConcreteValue, INT_1)
+		assertMayContainButNotCertain(t, listWithConcreteValue, ANY_SERIALIZABLE)
+		assertMayContainButNotCertain(t, listWithConcreteValue, ANY_INT)
+		assertCannotPossiblyContain(t, listWithConcreteValue, INT_2)
+
+		anyIntPair := NewList(ANY_INT, ANY_INT)
+		assertMayContainButNotCertain(t, anyIntPair, ANY_SERIALIZABLE)
+		assertMayContainButNotCertain(t, anyIntPair, ANY_INT)
+		assertMayContainButNotCertain(t, anyIntPair, INT_1)
+		assertMayContainButNotCertain(t, anyIntPair, INT_2)
+
+		concretizableList := NewList(INT_1, INT_2)
+		assertContains(t, concretizableList, INT_1)
+		assertContains(t, concretizableList, INT_2)
+		assertMayContainButNotCertain(t, concretizableList, ANY_SERIALIZABLE)
+		assertMayContainButNotCertain(t, concretizableList, ANY_INT)
+		assertCannotPossiblyContain(t, concretizableList, INT_3)
+	})
+}
+
+func TestDictionary(t *testing.T) {
 
 	t.Run("Test()", func(t *testing.T) {
 
