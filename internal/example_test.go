0835e884dcc60864c09a4ac9dd0511aa78a3924b
GraphR00t <113632189+GraphR00t@users.noreply.github.com>
2023-12-11 02:00:22 +0100

    examples: run most tested examples in parallel

--- internal/example_test.go
+++ internal/example_test.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"io"
@@ -24,7 +25,7 @@ import (
 	"github.com/inoxlang/inox/internal/globals/chrome_ns"
 	"github.com/inoxlang/inox/internal/globals/fs_ns"
 
-	"github.com/inoxlang/inox/internal/permkind"
+	_ "embed"
 
 	"github.com/inoxlang/inox/internal/utils"
 )
@@ -33,15 +34,33 @@ const (
 	DEFAULT_TEST_TIMEMOUT_DURATION = 10 * time.Second
 	CHROME_EXAMPLE_FOLDER          = "chrome/"
 	PROJECT_EXAMPLES_FOLDER        = "projects/"
+	EXAMPLES_DIR                   = "./examples"
 )
 
 var (
 	TIMING_OUT_EXAMPLES        = []string{"events.ix"}
 	CANCELLED_TOP_CTX_EXAMPLES = []string{"execution-time.ix", "rollback-on-cancellation.ix"}
-	SKIPPED_EXAMPLES           = []string{"get-resource.ix", "websocket.ix", "shared-patterns.ix", "add.ix", "models.ix"}
+	SKIPPED_EXAMPLES           = []string{"get-resource.ix", "websocket.ix", "shared-patterns.ix", "add.ix", "models.ix", "fs-events.ix"}
 
 	RUN_BROWSER_AUTOMATION_EXAMPLES = os.Getenv("RUN_BROWSER_AUTOMATION_EXAMPLES") == "true"
 )
+
+type filesystemWithRootWD struct {
+	afs.Filesystem
+}
+
+func (fls filesystemWithRootWD) Absolute(path string) (string, error) {
+	if strings.Contains(path, "/") {
+		_ = 1
+	}
+	if len(path) > 1 && path[0] == '.' && path[1] == '/' {
+		return path[1:], nil
+	}
+	if path[0] != '/' && !strings.HasPrefix(path, "../") {
+		return "/" + path, nil
+	}
+	return path, nil
+}
 
 // TestExamples tests the scripts located in ./examples/ .
 func TestExamples(t *testing.T) {
@@ -50,13 +69,53 @@ func TestExamples(t *testing.T) {
 	}
 
 	chrome_ns.AllowBrowserAutomation()
+	inProjectRoot := false
 
-	//we set the working directory to the project's root
 	dir, _ := os.Getwd()
 
-	defer os.Chdir(dir)
-	os.Chdir("..")
-	core.SetInitialWorkingDir(os.Getwd)
+	for _, entry := range utils.Must(os.ReadDir(dir)) {
+		if entry.Name() == "examples" {
+			inProjectRoot = true
+			break
+		}
+	}
+	exampleDir := "./examples"
+
+	if !inProjectRoot {
+		exampleDir = "." + EXAMPLES_DIR
+	}
+
+	//copy the examples folder inside a memory filesystem
+
+	memFS := fs_ns.NewMemFilesystem(10_000_000)
+	osFs := fs_ns.GetOsFilesystem()
+
+	var exampleFilePaths []string
+	const exampleDirInMemFS = "/examples/"
+
+	core.WalkDir(osFs, core.Path(exampleDir)+"/", func(path core.Path, d fs.DirEntry, err error) error {
+		if strings.HasPrefix(path.UnderlyingString(), "./../") {
+			path = path[2:]
+		}
+
+		newPath := filepath.Clean(strings.ReplaceAll(string(path), exampleDir, exampleDirInMemFS))
+
+		if d.IsDir() {
+			return memFS.MkdirAll(newPath, d.Type())
+		} else {
+			content, err := util.ReadFile(osFs, path.UnderlyingString())
+			if err != nil {
+				return err
+			}
+
+			if strings.HasSuffix(newPath, inoxconsts.INOXLANG_FILE_EXTENSION) {
+				exampleFilePaths = append(exampleFilePaths, newPath)
+				content = bytes.ReplaceAll(content, []byte(EXAMPLES_DIR), []byte(exampleDirInMemFS))
+			}
+
+			return util.WriteFile(memFS, newPath, content, d.Type())
+		}
+	})
 
 	//uncomment the following lines to test a given script
 	// testExample(t, exampleTestConfig{
@@ -69,34 +128,64 @@ func TestExamples(t *testing.T) {
 	//return
 
 	t.Run("tree-walk", func(t *testing.T) {
-		testExamples(t, false, false)
+		testExamples(testExamplesArgs{
+			t:                t,
+			memFS:            memFS,
+			exampleFilePaths: exampleFilePaths,
+		})
 	})
 
 	if !testing.Short() {
+
 		t.Run("bytecode", func(t *testing.T) {
-			testExamples(t, true, true)
+			testExamples(testExamplesArgs{
+				t:                t,
+				memFS:            memFS,
+				exampleFilePaths: exampleFilePaths,
+				useBytecode:      true,
+				optimizeBytecode: true,
+			})
 		})
 	}
 
 	t.Run("optimized bytecode", func(t *testing.T) {
-		testExamples(t, true, true)
+		testExamples(testExamplesArgs{
+			t:                t,
+			memFS:            memFS,
+			exampleFilePaths: exampleFilePaths,
+			useBytecode:      true,
+			optimizeBytecode: false,
+		})
 	})
 }
 
-func testExamples(t *testing.T, useBytecode, optimizeBytecode bool) {
+type testExamplesArgs struct {
+	t                             *testing.T
+	memFS                         *fs_ns.MemFilesystem
+	exampleFilePaths              []string
+	useBytecode, optimizeBytecode bool
+}
 
-	const exampleFolder = "./examples"
-	var exampleFilePaths []string
+func testExamples(args testExamplesArgs) {
+	t := args.t
+	memFS := args.memFS
 
-	filepath.Walk(exampleFolder, func(path string, info fs.FileInfo, err error) error {
-		if info.Mode().IsRegular() && strings.HasSuffix(path, inoxconsts.INOXLANG_FILE_EXTENSION) {
-			exampleFilePaths = append(exampleFilePaths, path)
-		}
-		return nil
+	fsSnapshot, err := memFS.TakeFilesystemSnapshot(core.FilesystemSnapshotConfig{
+		InclusionFilters: []core.PathPattern{"/..."},
+		GetContent: func(ChecksumSHA256 [32]byte) core.AddressableContent {
+			return nil
+		},
 	})
 
-	for _, fpath := range exampleFilePaths {
+	if !assert.NoError(args.t, err) {
+		return
+	}
+
+	for _, fpath := range args.exampleFilePaths {
+		fpath := fpath
 		testName := strings.ReplaceAll(fpath, "/", "--")
+
+		// check if the test should be executed
 
 		if strings.Contains(fpath, PROJECT_EXAMPLES_FOLDER) && !strings.HasSuffix(fpath, "/main.ix") {
 			continue
@@ -106,15 +195,25 @@ func testExamples(t *testing.T, useBytecode, optimizeBytecode bool) {
 			continue
 		}
 
+		content := utils.Must(util.ReadFile(memFS, fpath))
+
 		t.Run(testName, func(t *testing.T) {
+			//parallelize all tests that don't start an HTTP server
+			if !bytes.Contains(content, []byte("http.Server")) {
+				t.Parallel()
+			}
+
+			fls := filesystemWithRootWD{
+				Filesystem: utils.Must(fsSnapshot.NewAdaptedFilesystem(1_000_000)),
+			}
+
 			testExample(t, exampleTestConfig{
 				fpath:            fpath,
-				useBytecode:      useBytecode,
-				optimizeBytecode: optimizeBytecode,
+				useBytecode:      args.useBytecode,
+				optimizeBytecode: args.optimizeBytecode,
 				testTimeout:      DEFAULT_TEST_TIMEMOUT_DURATION,
-				runInTempDir:     true,
+				fls:              fls,
 			})
-
 		})
 	}
 
@@ -124,7 +223,7 @@ type exampleTestConfig struct {
 	fpath                         string
 	useBytecode, optimizeBytecode bool
 	testTimeout                   time.Duration
-	runInTempDir                  bool
+	fls                           afs.Filesystem
 }
 
 func testExample(t *testing.T, config exampleTestConfig) {
@@ -133,8 +232,6 @@ func testExample(t *testing.T, config exampleTestConfig) {
 	useBytecode := config.useBytecode
 	optimizeBytecode := config.optimizeBytecode
 	testTimeout := config.testTimeout
-	runInTempDir := config.runInTempDir
-
 	filename := filepath.Base(fpath)
 
 	if strings.Contains(fpath, CHROME_EXAMPLE_FOLDER) && !RUN_BROWSER_AUTOMATION_EXAMPLES {
@@ -142,7 +239,6 @@ func testExample(t *testing.T, config exampleTestConfig) {
 		return
 	}
 
-	osFilesystem := fs_ns.GetOsFilesystem()
 	var scriptContextFileSystem afs.Filesystem
 	var proj *project.Project
 
@@ -153,13 +249,13 @@ func testExample(t *testing.T, config exampleTestConfig) {
 		fileDir := core.Path(fileDirNoTrailingSlash) + "/"
 		scriptContextFileSystem = fs_ns.NewMemFilesystem(1_000_000)
 
-		core.WalkDir(osFilesystem, fileDir, func(path core.Path, d fs.DirEntry, err error) error {
+		core.WalkDir(config.fls, fileDir, func(path core.Path, d fs.DirEntry, err error) error {
 			pathInSnapshot := strings.TrimPrefix(string(path), fileDirNoTrailingSlash)
 
 			if d.IsDir() {
 				return scriptContextFileSystem.MkdirAll(pathInSnapshot, d.Type())
 			} else {
-				content, err := util.ReadFile(osFilesystem, path.UnderlyingString())
+				content, err := util.ReadFile(config.fls, path.UnderlyingString())
 				if err != nil {
 					return err
 				}
@@ -168,35 +264,13 @@ func testExample(t *testing.T, config exampleTestConfig) {
 		})
 
 		proj = project.NewDummyProject("project", scriptContextFileSystem.(core.SnapshotableFilesystem))
-	} else if runInTempDir {
-		scriptContextFileSystem = osFilesystem
-		tempDir := t.TempDir()
-
-		//we copy the examples in test directory and we set the WD to this directory
-
-		err := fs_ns.Copy(core.NewContext(core.ContextConfig{
-			Permissions: []core.Permission{
-				core.FilesystemPermission{Kind_: permkind.Read, Entity: core.PathPattern("/...")},
-				core.FilesystemPermission{Kind_: permkind.Create, Entity: core.PathPattern("/...")},
-				core.FilesystemPermission{Kind_: permkind.WriteStream, Entity: core.PathPattern("/...")},
-			},
-			Limits:     core.GetDefaultScriptLimits(),
-			Filesystem: scriptContextFileSystem,
-		}), core.Path("./examples/"), core.Path(filepath.Join(tempDir, "./examples/")+"/"))
-
-		if !assert.NoError(t, err) {
-			return
-		}
-
-		dir, _ := os.Getwd()
-
-		defer os.Chdir(dir)
-		os.Chdir(tempDir)
-		core.SetInitialWorkingDir(os.Getwd)
+	} else {
+		scriptContextFileSystem = config.fls
 	}
 
 	done := make(chan int)
 
+	//execute the example in a goroutine.
 	go func() {
 		defer func() {
 			if v := recover(); v != nil {
@@ -210,6 +284,7 @@ func testExample(t *testing.T, config exampleTestConfig) {
 		})
 
 		core.NewGlobalState(parsingCompilationContext)
+		defer parsingCompilationContext.CancelGracefully()
 
 		actualFpath := fpath
 		if proj != nil {
@@ -247,6 +322,7 @@ func testExample(t *testing.T, config exampleTestConfig) {
 		close(done)
 	}()
 
+	//wait for the example to finish.
 	select {
 	case <-done:
 		if utils.SliceContains(TIMING_OUT_EXAMPLES, filename) {
