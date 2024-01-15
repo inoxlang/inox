4e4a67a5921e55391bd6a2f34884d10bc8a001e9
GraphR00t <113632189+GraphR00t@users.noreply.github.com>
2024-01-15 18:04:16 +0100

    fix some default limit values

--- internal/globals/default_state.go
+++ internal/globals/default_state.go
@@ -85,14 +85,14 @@ var (
 		{Name: fs_ns.FS_READ_LIMIT_NAME, Kind: core.ByteRateLimit, Value: 10_000_000},
 		{Name: fs_ns.FS_WRITE_LIMIT_NAME, Kind: core.ByteRateLimit, Value: 10_000_000},
 
-		{Name: fs_ns.FS_NEW_FILE_RATE_LIMIT_NAME, Kind: core.FrequencyLimit, Value: 100},
+		{Name: fs_ns.FS_NEW_FILE_RATE_LIMIT_NAME, Kind: core.FrequencyLimit, Value: 100 * core.FREQ_LIMIT_SCALE},
 		{Name: fs_ns.FS_TOTAL_NEW_FILE_LIMIT_NAME, Kind: core.TotalLimit, Value: 1000},
 
-		{Name: http_ns.HTTP_REQUEST_RATE_LIMIT_NAME, Kind: core.FrequencyLimit, Value: 20},
+		{Name: http_ns.HTTP_REQUEST_RATE_LIMIT_NAME, Kind: core.FrequencyLimit, Value: 20 * core.FREQ_LIMIT_SCALE},
 		{Name: ws_ns.WS_SIMUL_CONN_TOTAL_LIMIT_NAME, Kind: core.TotalLimit, Value: 2},
 		{Name: net_ns.TCP_SIMUL_CONN_TOTAL_LIMIT_NAME, Kind: core.TotalLimit, Value: 2},
 
-		{Name: s3_ns.OBJECT_STORAGE_REQUEST_RATE_LIMIT_NAME, Kind: core.FrequencyLimit, Value: 10},
+		{Name: s3_ns.OBJECT_STORAGE_REQUEST_RATE_LIMIT_NAME, Kind: core.FrequencyLimit, Value: 10 * core.FREQ_LIMIT_SCALE},
 	}
 
 	_ = []core.GoValue{
