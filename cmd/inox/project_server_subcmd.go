eafc7aa967a73c4d33c0958ea0f7b208a2c64022
GraphR00t <113632189+GraphR00t@users.noreply.github.com>
2024-03-01 01:05:30 +0100

    projectserver: add a hyperscript parsing service

--- cmd/inox/project_server_subcmd.go
+++ cmd/inox/project_server_subcmd.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -15,9 +16,11 @@ import (
 	"github.com/inoxlang/inox/internal/config"
 	"github.com/inoxlang/inox/internal/core"
 	"github.com/inoxlang/inox/internal/core/permkind"
+	"github.com/inoxlang/inox/internal/deno"
 	"github.com/inoxlang/inox/internal/globals/chrome_ns"
 	"github.com/inoxlang/inox/internal/globals/fs_ns"
 	"github.com/inoxlang/inox/internal/globals/s3_ns"
+	"github.com/inoxlang/inox/internal/hyperscript/hsparse"
 	"github.com/inoxlang/inox/internal/inoxconsts"
 	"github.com/inoxlang/inox/internal/inoxd/node"
 	"github.com/inoxlang/inox/internal/inoxd/nodeimpl"
@@ -27,11 +30,14 @@ import (
 	"github.com/inoxlang/inox/internal/projectserver"
 	"github.com/inoxlang/inox/internal/projectserver/jsonrpc"
 	"github.com/inoxlang/inox/internal/utils"
+	"github.com/oklog/ulid/v2"
 	"github.com/rs/zerolog"
 )
 
 const (
 	LSP_SESSION_INIT_TIMEOUT = 2 * time.Second //The websocket is closed if the session is not initialized after this duration.
+
+	DENO_BINARY_LOCATION = "/tmp/service-deno"
 )
 
 func ProjectServer(mainSubCommand string, mainSubCommandArgs []string, outW, errW io.Writer) (exitCode int) {
@@ -120,34 +126,8 @@ func ProjectServer(mainSubCommand string, mainSubCommandArgs []string, outW, err
 	if projectServerConfig.AllowBrowserAutomation {
 		chrome_ns.AllowBrowserAutomation()
 
-		//download a chrome browser if not present.
-		//this is done synchronously because Landlock is invoked further in the code.
-		func() {
-			defer utils.Recover()
-
-			logger := zerolog.New(out).With().Str(core.SOURCE_LOG_FIELD_NAME, "browser-installation").Logger()
-			downloadCtx, cancel := context.WithTimeout(context.Background(), BROWSER_DOWNLOAD_TIMEOUT)
-			defer cancel()
-
-			if !projectServerConfig.IgnoreInstalledBrowser {
-				path, ok := chrome_ns.LookPath()
-				if ok {
-					logger.Info().Msgf("chrome browser found at %q\n", path)
-					chrome_ns.SetBrowserBinPath(path)
-					return
-				}
-			} else {
-				logger.Info().Msgf("any browser not installed by the project server will be ignored")
-			}
-
-			binpath, err := chrome_ns.DownloadBrowser(downloadCtx, logger)
-			if err != nil {
-				logger.Err(err).Msg("failed to download a browser")
-				return
-			}
-			logger.Info().Msgf("set browser binary path to %s", binpath)
-			chrome_ns.SetBrowserBinPath(binpath)
-		}()
+		//Download a chrome browser if not present. This is done synchronously because Landlock is invoked further in the code.
+		downloadChromeBrowser(out, projectServerConfig)
 	}
 
 	//create context & state
@@ -257,15 +237,14 @@ func ProjectServer(mainSubCommand string, mainSubCommandArgs []string, outW, err
 		node.SetAgent(nodeAgent)
 	}
 
-	//Start the browser proxy.
-
-	if projectServerConfig.AllowBrowserAutomation {
-		err = chrome_ns.StartSharedProxy(ctx)
+	go func() {
+		err := startAdjacentServices(ctx, projectServerConfig)
 		if err != nil {
-			fmt.Fprintln(errW, "failed to start shared browser proxy:", err)
-			return ERROR_STATUS_CODE
+			fmt.Fprintln(errW, err)
+			fmt.Fprintln(errW, "cancel the root context because some adjacent services failed to start")
+			ctx.CancelGracefully()
 		}
-	}
+	}()
 
 	//Start the project server and development servers.
 
@@ -309,6 +288,10 @@ func determineProjectServerPermissions(projectServerConfig projectserver.Individ
 		//Lighweight thread permissions
 
 		core.LThreadPermission{Kind_: permkind.Create},
+
+		//Command permissions
+
+		core.CommandPermission{CommandName: core.String(DENO_BINARY_LOCATION)}, //We need the permission because of landlock.
 	}
 
 	perms = append(perms, core.GetDefaultGlobalVarPermissions()...)
@@ -353,4 +336,107 @@ func determineProjectServerPermissions(projectServerConfig projectserver.Individ
 	}
 
 	return perms
+}
+
+func downloadChromeBrowser(out io.Writer, projectServerConfig projectserver.IndividualServerConfig) {
+	defer utils.Recover()
+
+	logger := zerolog.New(out).With().Str(core.SOURCE_LOG_FIELD_NAME, "browser-installation").Logger()
+	downloadCtx, cancel := context.WithTimeout(context.Background(), BROWSER_DOWNLOAD_TIMEOUT)
+	defer cancel()
+
+	if !projectServerConfig.IgnoreInstalledBrowser {
+		path, ok := chrome_ns.LookPath()
+		if ok {
+			logger.Info().Msgf("chrome browser found at %q\n", path)
+			chrome_ns.SetBrowserBinPath(path)
+			return
+		}
+	} else {
+		logger.Info().Msgf("any browser not installed by the project server will be ignored")
+	}
+
+	binpath, err := chrome_ns.DownloadBrowser(downloadCtx, logger)
+	if err != nil {
+		logger.Err(err).Msg("failed to download a browser")
+		return
+	}
+	logger.Info().Msgf("set browser binary path to %s", binpath)
+	chrome_ns.SetBrowserBinPath(binpath)
+}
+
+func startAdjacentServices(ctx *core.Context, projectServerConfig projectserver.IndividualServerConfig) error {
+	//Start the browser proxy.
+
+	if projectServerConfig.AllowBrowserAutomation {
+		err := chrome_ns.StartSharedProxy(ctx)
+		if err != nil {
+			return fmt.Errorf("failed to start shared browser proxy: %w", err)
+		}
+	}
+
+	//Start some Deno services for internal use.
+
+	{
+		controlServerCtx := ctx.BoundChildWithOptions(core.BoundChildContextOptions{
+			Limits: []core.Limit{
+				utils.Must(core.GetLimit(ctx, fs_ns.FS_TOTAL_NEW_FILE_LIMIT_NAME, core.Int(10_000))),
+				utils.Must(core.GetLimit(ctx, fs_ns.FS_NEW_FILE_RATE_LIMIT_NAME, core.Frequency(10*core.FREQ_LIMIT_SCALE))),
+				utils.Must(core.GetLimit(ctx, fs_ns.FS_WRITE_LIMIT_NAME, core.ByteRate(10_000_000))),
+				utils.Must(core.GetLimit(ctx, fs_ns.FS_READ_LIMIT_NAME, core.ByteRate(10_000_000))),
+			},
+		})
+		controlServer, err := deno.NewControlServer(controlServerCtx, deno.ControlServerConfig{
+			Port: inoxconsts.DEFAULT_DENO_CONTROL_SERVER_PORT_INT_FOR_PROJECT_SERVER,
+		})
+
+		if err != nil {
+			return fmt.Errorf("faailed to create control server for Deno processes: %w", err)
+		}
+
+		earlyErrChan := make(chan error)
+		go func() {
+			defer utils.Recover()
+			earlyErrChan <- controlServer.Start()
+		}()
+
+		select {
+		case err = <-earlyErrChan:
+			if err != nil {
+				return fmt.Errorf("failed to start control server for Deno processes: %w", err)
+			}
+		case <-time.After(100 * time.Millisecond):
+		}
+
+		//Start the hyperscript parsing service.
+
+		startService := func(program string) (ulid.ULID, error) {
+			return controlServer.StartServiceProcess(controlServerCtx, deno.ServiceConfiguration{
+				RequiresPersistendWorkdir: false,
+				Name:                      "hyperscript-parser",
+				DenoBinaryLocation:        DENO_BINARY_LOCATION,
+				ServiceProgram:            hsparse.DENO_SERVICE_TS,
+				AllowNetwork:              true,
+				AllowLocalhostAccess:      true,
+			})
+		}
+
+		err = hsparse.StartHyperscriptParsingService(startService, func(ctx context.Context, input string, serviceID ulid.ULID) (json.RawMessage, error) {
+			process, ok := controlServer.GetServiceProcessByID(serviceID)
+			if !ok {
+				return nil, errors.New("service not found")
+			}
+
+			return process.CallMethod(controlServerCtx, hsparse.HYPERSCRIPT_PARSING_FUNCTION_NAME, map[string]any{
+				"input":                input,
+				"doNotIncludeNodeData": true,
+			})
+		})
+
+		if err != nil {
+			return fmt.Errorf("failed to start the hyperscript parsing service: %w", err)
+		}
+	}
+
+	return nil
 }
