d244f2d3555ff5bd19e688ab90293bce64bc2dd2
GraphR00t <113632189+GraphR00t@users.noreply.github.com>
2024-05-09 17:47:56 +0200

    hyperscript/analysis: add moire tests

--- internal/hyperscript/hsanalysis/analyze_test.go
+++ internal/hyperscript/hsanalysis/analyze_test.go
@@ -228,6 +228,34 @@ func TestAnalyzeClientSideAttributeInterpolation(t *testing.T) {
 
 	parse.RegisterParseHypercript(hsparse.ParseHyperScriptProgram)
 
+	t.Run("defined element-scoped variable", func(t *testing.T) {
+		chunk := parse.MustParseChunkSource(parse.InMemorySource{
+			NameString: "test",
+			CodeString: `<div class="A" x-for=":a in :list" y="((:a))"> </div> `,
+		})
+
+		strLit := parse.FindNodes(chunk.Node, (*parse.DoubleQuotedStringLiteral)(nil), nil)[2]
+		hyperscriptExpr := utils.Ret0OutOf3(hsparse.ParseHyperScriptExpression(context.Background(), ":a")).NodeData
+
+		errors, warnings, err := Analyze(Parameters{
+			LocationKind: locationKind,
+			Component: &Component{
+				Name:                        "A",
+				InitialElementScopeVarNames: []string{":a"},
+			},
+			Chunk:               chunk,
+			CodeStartIndex:      strLit.Span.Start + 3,
+			ProgramOrExpression: hyperscriptExpr,
+		})
+
+		if !assert.NoError(t, err) {
+			return
+		}
+
+		assert.Empty(t, warnings)
+		assert.Empty(t, errors)
+	})
+
 	t.Run("probably not-defined element-scoped variable", func(t *testing.T) {
 		chunk := parse.MustParseChunkSource(parse.InMemorySource{
 			NameString: "test",
@@ -274,6 +302,127 @@ func TestAnalyzeClientSideAttributeInterpolation(t *testing.T) {
 			},
 			Chunk:               chunk,
 			CodeStartIndex:      strLit.Span.Start + 3,
+			ProgramOrExpression: hyperscriptExpr,
+		})
+
+		if !assert.NoError(t, err) {
+			return
+		}
+
+		assert.Empty(t, warnings)
+		assert.Empty(t, errors)
+	})
+
+	t.Run("reference to an attribute that is not initialized", func(t *testing.T) {
+		chunk := parse.MustParseChunkSource(parse.InMemorySource{
+			NameString: "test",
+			CodeString: `<div class="A" y="((@data-x))">  </div> `,
+		})
+
+		strLit := parse.FindNodes(chunk.Node, (*parse.DoubleQuotedStringLiteral)(nil), nil)[1]
+		hyperscriptExpr := utils.Ret0OutOf3(hsparse.ParseHyperScriptExpression(context.Background(), "@data-x")).NodeData
+
+		errors, warnings, err := Analyze(Parameters{
+			LocationKind: locationKind,
+			Component: &Component{
+				Name: "A",
+			},
+			Chunk:               chunk,
+			CodeStartIndex:      strLit.Span.Start + 3,
+			ProgramOrExpression: hyperscriptExpr,
+		})
+
+		if !assert.NoError(t, err) {
+			return
+		}
+
+		assert.Empty(t, warnings)
+		assert.Equal(t, []Error{
+			MakeError(text.FmtAttributeMayNotBeInitialized("data-x", true), chunk.GetSourcePosition(parse.NodeSpan{Start: 20, End: 27})),
+		}, errors)
+	})
+}
+
+func TestAnalyzeClientSideTextInterpolation(t *testing.T) {
+
+	locationKind := ClientSideTextInterpolation
+
+	parse.RegisterParseHypercript(hsparse.ParseHyperScriptProgram)
+
+	t.Run("defined element-scoped variable", func(t *testing.T) {
+		chunk := parse.MustParseChunkSource(parse.InMemorySource{
+			NameString: "test",
+			CodeString: `<div class="A" x-for=":a in :list"> ((:a)) </div> `,
+		})
+
+		markupText := parse.FindFirstNode(chunk.Node, (*parse.MarkupText)(nil))
+		hyperscriptExpr := utils.Ret0OutOf3(hsparse.ParseHyperScriptExpression(context.Background(), ":a")).NodeData
+
+		errors, warnings, err := Analyze(Parameters{
+			LocationKind: locationKind,
+			Component: &Component{
+				Name:                        "A",
+				InitialElementScopeVarNames: []string{":a"},
+			},
+			Chunk:               chunk,
+			CodeStartIndex:      markupText.Span.Start + 3,
+			ProgramOrExpression: hyperscriptExpr,
+		})
+
+		if !assert.NoError(t, err) {
+			return
+		}
+
+		assert.Empty(t, warnings)
+		assert.Empty(t, errors)
+	})
+
+	t.Run("probably not-defined element-scoped variable", func(t *testing.T) {
+		chunk := parse.MustParseChunkSource(parse.InMemorySource{
+			NameString: "test",
+			CodeString: `<div class="A"> ((:a)) </div> `,
+		})
+
+		markupText := parse.FindFirstNode(chunk.Node, (*parse.MarkupText)(nil))
+		hyperscriptExpr := utils.Ret0OutOf3(hsparse.ParseHyperScriptExpression(context.Background(), ":a")).NodeData
+
+		errors, warnings, err := Analyze(Parameters{
+			LocationKind: locationKind,
+			Component: &Component{
+				Name: "A",
+			},
+			Chunk:               chunk,
+			CodeStartIndex:      markupText.Span.Start + 3,
+			ProgramOrExpression: hyperscriptExpr,
+		})
+
+		if !assert.NoError(t, err) {
+			return
+		}
+
+		assert.Empty(t, warnings)
+		assert.Equal(t, []Error{
+			MakeError(text.FmtElementScopeVarMayNotBeDefined(":a", true), chunk.GetSourcePosition(parse.NodeSpan{Start: 18, End: 20})),
+		}, errors)
+	})
+
+	t.Run("reference to initialized attribute", func(t *testing.T) {
+		chunk := parse.MustParseChunkSource(parse.InMemorySource{
+			NameString: "test",
+			CodeString: `<div class="A"> ((@data-x)) </div> `,
+		})
+
+		markupText := parse.FindFirstNode(chunk.Node, (*parse.MarkupText)(nil))
+		hyperscriptExpr := utils.Ret0OutOf3(hsparse.ParseHyperScriptExpression(context.Background(), "@data-x")).NodeData
+
+		errors, warnings, err := Analyze(Parameters{
+			LocationKind: locationKind,
+			Component: &Component{
+				Name:                          "A",
+				InitializedDataAttributeNames: []string{"data-x"},
+			},
+			Chunk:               chunk,
+			CodeStartIndex:      markupText.Span.Start + 3,
 			ProgramOrExpression: hyperscriptExpr,
 		})
 
