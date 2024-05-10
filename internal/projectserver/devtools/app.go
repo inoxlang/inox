882afb3f13d809cb1094cdce0be7486b36d7a7da
GraphR00t <113632189+GraphR00t@users.noreply.github.com>
2024-05-10 21:10:00 +0200

    devtools: create ./dev/tools/ with default tools if the dir is not present

--- internal/projectserver/devtools/app.go
+++ internal/projectserver/devtools/app.go
@@ -1,12 +1,16 @@
 package devtools
 
 import (
+	"embed"
 	"errors"
 	"fmt"
 	"io"
 	"io/fs"
+	"path/filepath"
+	"strings"
 	"time"
 
+	"github.com/go-git/go-billy/v5/util"
 	"github.com/inoxlang/inox/internal/core"
 	"github.com/inoxlang/inox/internal/core/slog"
 	"github.com/inoxlang/inox/internal/globals/http_ns"
@@ -25,19 +29,53 @@ var (
 	ErrInexistingOrInvalidDevToolsEntryPoint = errors.New("entry point for the dev tools server does not exist or is invalid")
 )
 
+var (
+	//go:embed tools/**
+	DEFAULT_TOOLS_DIR embed.FS
+)
+
 // StartWebApp starts an Inox web application that provides development tools.
 // The application servers listens on a development port on localhost, therefore it is virtual and do not directly bind.
 func (inst *Instance) StartWebApp() error {
+
+	const DEFAULT_DIR_FPERMS = 0600
+	const DEFAULT_FILE_FPERMS = 0600
 
 	_, ok := http_ns.GetDevServer(inst.toolsServerPort)
 	if !ok {
 		return fmt.Errorf("failed to start dev tools server: dev server on port %s is not listening", inst.toolsServerPort)
 	}
 
+	_, err := inst.developerWorkingFS.Stat(layout.DEV_TOOLS_DIR_PATH)
+	if err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			//If the directory does not exist, create it and includes ./tools.
+			err = inst.developerWorkingFS.MkdirAll(layout.DEV_TOOLS_DIR_PATH, DEFAULT_DIR_FPERMS)
+			if err != nil {
+				return err
+			}
+
+			err = fs.WalkDir(DEFAULT_TOOLS_DIR, "tools", func(path string, d fs.DirEntry, err error) error {
+				actualPath := filepath.Join(layout.DEV_TOOLS_DIR_PATH, strings.TrimPrefix(path, "tools"))
+
+				if d.IsDir() {
+					return inst.developerWorkingFS.MkdirAll(actualPath, DEFAULT_DIR_FPERMS)
+				} else {
+					content := utils.Must(DEFAULT_TOOLS_DIR.ReadFile(path))
+					return util.WriteFile(inst.developerWorkingFS, actualPath, content, DEFAULT_FILE_FPERMS)
+				}
+			})
+		}
+		if err != nil {
+			return err
+		}
+	}
+
 	entryPoint := layout.DEV_TOOLS_SERVER_ENTRY_POINT
 	stat, err := inst.developerWorkingFS.Stat(entryPoint)
 	if err != nil {
 		if errors.Is(err, fs.ErrNotExist) {
+			//We do not create the entrypoint because custom dev tools may have been installed.
 			return ErrInexistingOrInvalidDevToolsEntryPoint
 		}
 		return err
