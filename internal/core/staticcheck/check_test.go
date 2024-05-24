c5115ec37669680a2c6b9ebb594495b7545a81f9
GraphR00t <113632189+GraphR00t@users.noreply.github.com>
2024-05-24 20:02:58 +0200

    staticcheck: remove a test on test cases

--- internal/core/staticcheck/check_test.go
+++ internal/core/staticcheck/check_test.go
@@ -3053,22 +3053,6 @@ func TestCheck(t *testing.T) {
 			assert.NoError(t, staticCheckNoData(StaticCheckInput{Node: n, Chunk: src}))
 		})
 
-		t.Run("should not inherit the `dbs` global", func(t *testing.T) {
-			n, src := mustParseCode(`
-				globalvar dbs = {}
-				return testcase {
-					dbs
-				}
-			`)
-
-			identLiteral := ast.FindNodes(n, (*ast.IdentifierLiteral)(nil), nil)[1]
-
-			err := staticCheckNoData(StaticCheckInput{Node: n, Chunk: src})
-			expectedErr := utils.CombineErrors(
-				makeError(identLiteral, src, text.FmtVarIsNotDeclared("dbs")),
-			)
-			assert.Equal(t, expectedErr, err)
-		})
 	})
 
 	t.Run("inclusion import statement", func(t *testing.T) {
